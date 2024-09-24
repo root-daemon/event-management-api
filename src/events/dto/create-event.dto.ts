da7dd1caf6bafa489e98cd7f9b604b1f1e5691d3
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-09-24 12:33:16 +0530

    feat: docs

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,7 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 // src/events/dto/create-event.dto.ts
 export class CreateEventDto {
+  @ApiProperty({
+    title: 'Name',
+    example: 'Event Name',
+    description: 'The name of the event',
+  })
   name: string;
+  @ApiProperty({
+    title: 'Location',
+    example: 'Chennai, TN',
+    description: 'The location of the event',
+  })
   location: string;
+  @ApiProperty({
+    title: 'Time',
+    example: '2024-09-24T06:59:11.668Z',
+    description: 'The time of the event',
+  })
   time: Date;
+  @ApiProperty({
+    title: 'Description',
+    example: 'Description of the event',
+    description: 'The Description of the event',
+  })
   description: string;
 }
--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -1,4 +1,37 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEventDto } from './create-event.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+export class UpdateEventDto extends PartialType(CreateEventDto) {
+  @ApiProperty({
+    title: 'Name',
+    example: 'Event Name',
+    description: 'The name of the event',
+    required: false,
+  })
+  name?: string;
+
+  @ApiProperty({
+    title: 'Location',
+    example: 'Chennai, TN',
+    description: 'The location of the event',
+    required: false,
+  })
+  location?: string;
+
+  @ApiProperty({
+    title: 'Time',
+    example: '2024-09-24T06:59:11.668Z',
+    description: 'The time of the event',
+    required: false,
+  })
+  time?: Date;
+
+  @ApiProperty({
+    title: 'Description',
+    example: 'Description of the event',
+    description: 'The Description of the event',
+    required: false,
+  })
+  description?: string;
+}
